18b289e880fbcff6a6c66e038b86fd89bd197b80
Graham Hewett <graham@Nimlbes-MacBook-Pro.local>
2020-02-21 16:55:34 +0000

    refactor image test component

--- tests/integration/components/rental/image-test.js
+++ tests/integration/components/rental/image-test.js
@@ -28,17 +28,12 @@ module('Integration | Component | rental/image', function(hooks) {
     `);
 
     assert.dom('button.image').exists();
-
     assert.dom('.image').doesNotHaveClass('large');
     assert.dom('.image small').hasText('View Larger');
-
     await click('button.image');
-
     assert.dom('.image').hasClass('large')
     assert.dom('.image small').hasText('View Smaller');
-
     await click('button.image');
-
     assert.dom('.image').doesNotHaveClass('large');
     assert.dom('.image small').hasText('View Larger');
   });
